cdec7dd4d54be86c9c837a04357bf3d6ca985e9e
AyoubS-afk <saidiayoubmohammed@gmail.com>
2023-04-08 16:37:00 +0200

    h

--- class_types/overlay_types.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class OverlayTypes(Enum):
-    DEFAULT = 1
-    FIRE = 2
-    DESTRUCTION = 3
--- class_types/tile_types.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class TileTypes(Enum):
-    GRASS = 1
-    WATER = 2
-    WHEAT = 3
