a76a9cc373947fbb10b097015bf7708bb0dce5dc
AyoubS-afk <saidiayoubmohammed@gmail.com>
2023-04-08 16:56:13 +0200

    n

--- class_types/orientation_types.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class OrientationTypes(Enum):
-    TOP_LEFT = 1
-    TOP_RIGHT = 2
-    BOTTOM_RIGHT = 3
-    BOTTOM_LEFT = 4
--- walkers/final/tax_collector.py
+++
@@ -1,9 +0,0 @@
-from buildable.buildable import Buildable
-from class_types.walker_types import WalkerTypes
-from walkers.walker import Walker
-
-
-class TaxCollector(Walker):
-    def __init__(self, associated_building: 'Buildable'):
-        super().__init__(WalkerTypes.TAX_COLLECTOR, associated_building)
-
